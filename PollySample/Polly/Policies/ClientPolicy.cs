6e5f77917f18bd6c4de4d7983435437e0816642e
PyFaNNy <fanny.koob@gmail.com>
2022-11-04 23:30:55 +0300

    Implement sample for Polly nuget

---
+++ PollySample/Polly/Policies/ClientPolicy.cs
@@ -0,0 +1,33 @@
+﻿using Polly;
+using Polly.CircuitBreaker;
+using Polly.Retry;
+
+namespace RequestService.Policies
+{
+    public class ClientPolicy
+    {
+        public AsyncRetryPolicy<HttpResponseMessage> ImmediateHttpRetry { get; }
+        public AsyncRetryPolicy<HttpResponseMessage> LinearHttpRetry { get; }
+        public AsyncRetryPolicy<HttpResponseMessage> ExponentialHttpRetry { get; }
+        public AsyncCircuitBreakerPolicy<HttpResponseMessage> CircuitBreakerPolicy { get; }
+
+        public ClientPolicy()
+        {
+            ImmediateHttpRetry = Policy.HandleResult<HttpResponseMessage>(
+                res => !res.IsSuccessStatusCode)
+                .RetryAsync(10);
+
+            LinearHttpRetry = Policy.HandleResult<HttpResponseMessage>(
+                res => !res.IsSuccessStatusCode)
+                .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(3));
+
+            ExponentialHttpRetry = Policy.HandleResult<HttpResponseMessage>(
+                res => !res.IsSuccessStatusCode)
+                .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+
+            CircuitBreakerPolicy = Policy.HandleResult<HttpResponseMessage>(
+                res => !res.IsSuccessStatusCode)
+                .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30));
+        }
+    }
+}
